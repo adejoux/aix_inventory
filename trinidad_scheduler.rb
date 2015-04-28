80a3595bb9cfdf2aae10dd7f53c2513ea7f9d237
Alain Dejoux <adejoux@krystalia.net>
2015-04-28 15:14:18 +0200

    moved to whenever scheduler

--- trinidad_scheduler.rb
+++
@@ -1,6 +0,0 @@
-class ImportJob < TrinidadScheduler.Cron "0 0/5 * * * ?"
-  def run
-    Delayed::Job.enqueue XmlServerImportWorker.new
-    Delayed::Job.enqueue CsvServerImportWorker.new
-  end
-end
